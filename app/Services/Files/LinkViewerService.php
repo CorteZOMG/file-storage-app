83669f6d080fb733093b455127895b1906693de6
CorteZOMG <artem.luchak@gmail.com>
2026-04-14 20:38:14 +0300

    feat(ST-38): create share link logic with views and routes

---
+++ app/Services/Files/LinkViewerService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services\Files;
+
+use App\Models\ShareLink;
+
+class LinkViewerService
+{
+    /**
+     * Record a view for a given share link safely.
+     * 
+     * @param ShareLink $link
+     * @return void
+     */
+    public function recordView(ShareLink $link): void
+    {
+        $link->increment('views');
+    }
+}
