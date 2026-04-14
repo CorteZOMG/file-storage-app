39a083295c6c51baf4cdd0878de6fef4313b6f0f
CorteZOMG <artem.luchak@gmail.com>
2026-04-14 23:11:59 +0300

    feat(ST-51): add soft deletes to the table

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['user_id', 'name', 'path', 'comment', 'expires_at'])]
 class File extends Model
 {
+    use SoftDeletes;
+
     protected function casts(): array
     {
         return [
