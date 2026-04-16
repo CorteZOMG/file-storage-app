edbc0b8085b2036d5aaacdd664a9b70eabbc08aa
CorteZOMG <artem.luchak@gmail.com>
2026-04-16 13:43:17 +0300

    feat(ST-62): hook up routes for API

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FileApiController;
+use App\Http\Controllers\Api\ApiShareLinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Public Endpoints
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Public link view with no auth
+Route::get(
+    '/share/{token}',
+    [ApiShareLinkController::class, 'show']
+);
 
 // Protected Endpoints
 Route::middleware('auth:sanctum')->group(function () {
@@ -15,4 +23,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::apiResource('files', FileApiController::class)->except(['update']);
+
+    Route::post(
+        'files/{file}/links',
+        [ApiShareLinkController::class, 'store']
+    );
 });
